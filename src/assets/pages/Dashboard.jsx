07f9920519e5e12b84b72a52b538871d3138c499
Aayush Laddha <aayushladdha001@gmail.com>
2025-07-08 22:46:52 +0530

    .

--- src/assets/pages/Dashboard.jsx
+++ src/assets/pages/Dashboard.jsx
@@ -10,8 +10,8 @@ const Dashboard = () => {
   return (
     <>
       <Navbar />
-      <div className='gap-5 flex p-5 h-full w-full'>
-        <div className='border border-text w-[70%] h-fit flex flex-col rounded-b-lg'>
+      <div className='gap-5 flex flex-col md:flex-row p-5 h-full w-full'>
+        <div className='border border-text md:w-[70%] h-fit flex flex-col rounded-b-lg'>
           <section className='z-10 w-full h-100 bg-cyan-500'>
             <MapView />
           </section>
@@ -21,7 +21,7 @@ const Dashboard = () => {
           </section>
         </div>
 
-        <div className='w-[30%] rounded-lg'>
+        <div className='w-full md:w-[30%] rounded-lg'>
           <StatsCards />
         </div>
       </div>
